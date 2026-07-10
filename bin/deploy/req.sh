1d7779d624489860de579506208528d1f3940376
Artur Lissin <artdotlis@pm.me>
2026-07-10 12:49:43 +0000

    build: correct production deployment

--- bin/deploy/req.sh
+++ bin/deploy/req.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 echo "update"
 dnf -y update
 echo "installing requirements"
-dnf -y install python3 python3-pip git git-lfs make wget coreutils-single jq vim which
+dnf -y install python3.13 python3.13-pip git git-lfs make wget coreutils-single jq vim which
 dnf -y group install "Development Tools"
 dnf -y install bzip2-devel ncurses-devel libffi-devel \
     readline-devel openssl-devel sqlite-devel tk-devel
-ln -s /usr/bin/python3 /usr/bin/python
+ln -s /usr/bin/python3.13 /usr/bin/python
 dnf -y install nodejs libatomic
 echo "requirements installed"
